9b313664a80d49ea0d8a53aa729cc5f987e3ac94
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2022-08-12 19:14:11 +0300

    app connected to api

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const fetch = require('node-fetch');
 const express = require('express');
+const getVideoId = require('./utils/getVideoId');
 require('dotenv').config();
 const app = express();
 
@@ -13,9 +15,33 @@ app.set('view engine', 'ejs');
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { success: undefined, message: '' });
 });
 
+app.post('/', async (req, res) => {
+    const url = await req.body.videoUrl;
+    const videoId = await getVideoId(url);
+
+    if (videoId === undefined || videoId === "" || videoId === null) {
+        return res.render('index', { success: false, message: 'Please enter a video url.' });
+    } else {
+        const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': process.env.API_KEY,
+                'X-RapidAPI-Host': process.env.API_HOST
+            }
+        });
+
+        const response = await fetchAPI.json();
+
+        if (response.status === 'ok') {
+            return res.render('index', { success: true, song_link: response.link, song_title: response.title });
+        } else {
+            return res.render('index', { success: false, message: response.msg });
+        }
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ utils/getVideoId.js
@@ -0,0 +1,10 @@
+const getVideoId = (url) => {
+    // Our regex pattern to look for a youTube ID
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+    //Match the url with the regex
+    const match = url.match(regExp);
+    //Return the result
+    return match && match[2].length === 11 ? match[2] : undefined;
+};
+
+module.exports = getVideoId;
